a6f984d06424f563d16e52a7678f14129a77acc0
ledeanri <ledeanri@gmail.com>
2021-06-26 19:11:53 -0300

    realizadas alterações em views, criado tela de cadastro de conta

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,27 @@ class AccountsController < ApplicationController
     @accounts = Account.all
   end
 
+  def new
+    @user = current_user
+  end
+
+  def create
+    @account = Account.new(params.require(:account).permit(:amount))
+    @account.user = current_user
+    @account.save
+    redirect_to root_path
+  end
+
+  def edit
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @tarefa = Account.find(params[:id])
+    @tarefa.update(params.require(:account).permit(:user, :amount))
+    redirect_to root_path
+  end
+
   def show
     @account = Account.find_by_id(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
-  resources :accounts, only: [:index, :show]
+  resources :accounts, only: [:index, :show, :new, :edit, :create]
 end
